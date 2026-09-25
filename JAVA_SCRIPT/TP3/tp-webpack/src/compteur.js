9ff21a6a1894d3ca252b4d3f60a3d50cab389b6e
Bakame03 <nganjialdoalex@gmail.com>
2026-09-25 14:44:13 +0200

    feat: add Compteur class and interactive click counter button

--- JAVA_SCRIPT/TP3/tp-webpack/src/compteur.js
+++ JAVA_SCRIPT/TP3/tp-webpack/src/compteur.js
@@ -1,1 +1,12 @@
-        
+        export class Compteur {
+  #valeur = 0
+
+  incrementer () {
+    this.#valeur += 1
+    return this.#valeur
+  }
+
+  get valeur () {
+    return this.#valeur
+  }
+}
--- JAVA_SCRIPT/TP3/tp-webpack/src/index.js
+++ JAVA_SCRIPT/TP3/tp-webpack/src/index.js
@@ -1,6 +1,7 @@
 import './style.css'
 import logo from './logo.png'
 import { saluer } from './salutation.js'
+import { Compteur } from './compteur.js'
 
 const titre = document.createElement('h1')
 titre.textContent = saluer('webpack')
@@ -10,3 +11,11 @@ const image = new Image()
 image.src = logo
 image.alt = 'Logo'
 document.body.append(image)
+
+const compteur = new Compteur()
+const bouton = document.createElement('button')
+bouton.textContent = 'Clics : 0'
+bouton.addEventListener('click', () => {
+  bouton.textContent = `Clics : ${compteur.incrementer()}`
+})
+document.body.append(bouton)
