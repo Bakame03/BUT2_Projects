52375201d0d975ac67be87f719883327d95ebf4e
Bakame03 <nganjialdoalex@gmail.com>
2026-09-25 14:42:53 +0200

    feat: configure webpack asset modules and add style and image resources

--- JAVA_SCRIPT/TP3/tp-webpack/src/index.js
+++ JAVA_SCRIPT/TP3/tp-webpack/src/index.js
@@ -1,5 +1,12 @@
+import './style.css'
+import logo from './logo.png'
 import { saluer } from './salutation.js'
 
 const titre = document.createElement('h1')
 titre.textContent = saluer('webpack')
 document.body.append(titre)
+
+const image = new Image()
+image.src = logo
+image.alt = 'Logo'
+document.body.append(image)
--- JAVA_SCRIPT/TP3/tp-webpack/webpack.config.js
+++ JAVA_SCRIPT/TP3/tp-webpack/webpack.config.js
@@ -6,5 +6,23 @@ export default {
   output: {
     filename: 'main.js',
     path: path.resolve(import.meta.dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[contenthash][ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|ttf|otf|eot)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[contenthash][ext]'
+        }
+      }
+    ]
   }
 }
